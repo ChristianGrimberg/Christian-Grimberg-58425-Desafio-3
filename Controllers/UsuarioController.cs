f6f5e7d5a8085caf47ab90ef0e72903d693e3892
Christian Grimberg <christian.grimberg@outlook.com>
2024-06-16 12:24:39 +0000

    :computer: Se agrega el controlador de Usuario con un metodo GET de pruebas.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Christian_Grimberg_58425_Desafio_3.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+	[HttpGet(Name = "GetUsuario")]
+	public string GetUsuario()
+	{
+		return "Prueba";
+	}
+}
