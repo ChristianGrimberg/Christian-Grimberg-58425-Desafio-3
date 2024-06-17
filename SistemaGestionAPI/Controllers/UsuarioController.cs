5934cfc6fdb46f2394b69dee65ab4e28dc793f8f
Christian Grimberg <christian.grimberg@outlook.com>
2024-06-17 01:19:46 +0000

    :computer: Se traslada el proyecto WebAPI en un nuevo directorio y se utiliza appsettings.json para la cadena de conexión a la base de datos.
    Signed-off-by: Christian Grimberg <christian.grimberg@outlook.com>

---
+++ SistemaGestionAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+using Microsoft.AspNetCore.Mvc;
+
+using SistemaGestionBussiness;
+
+using SistemaGestionData;
+
+using SistemaGestionEntities;
+
+namespace Christian_Grimberg_58425_Desafio_3.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsuarioController : ControllerBase
+{
+  [HttpGet]
+  public IEnumerable<Usuario> GetUsuarios()
+  {
+    return UsuarioBussiness.ListarUsuarios(Connection.DatabaseConnection).ToArray();
+  }
+}
---
+++ SistemaGestionAPI/Models/Connection.cs
@@ -0,0 +1,8 @@
+﻿using System.Data.SqlClient;
+
+namespace Christian_Grimberg_58425_Desafio_3;
+
+public static class Connection
+{
+	public static SqlConnection? DatabaseConnection;
+}
